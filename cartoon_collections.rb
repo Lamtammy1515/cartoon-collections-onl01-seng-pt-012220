9d66fe9630f0c0d0f99b018e28c6506bfad50ec6
Tammy Lam <lamtammy1515@gmail.com>
2020-01-30 02:45:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,8 @@ def summon_captain_planet(array)
   array.collect {|elements| elements.capitalize! + "!"}
 end
 
-def long_planeteer_calls(calls)
-   
+def long_planeteer_calls(array)
+   array.any? {|element| element.length>4}
 end
  
 
