2f078af878191c0143ea9efd47cbad7cf5e65ca4
Tammy Lam <lamtammy1515@gmail.com>
2020-01-30 02:41:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,8 @@ def roll_call_dwarves(dwarves) # code an argument here
 end
 
 def summon_captain_planet(array) 
-  array.collect do |elements| 
-    return elements.capitalize!+"!"
+  array.collect do |elements|
+    return elements.capitalize! + "!"
   end 
 end
 
@@ -15,9 +15,9 @@ def long_planeteer_calls(calls)
 end
  
 
-
+end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
- 
+  print array.include? cheese_types
 end
